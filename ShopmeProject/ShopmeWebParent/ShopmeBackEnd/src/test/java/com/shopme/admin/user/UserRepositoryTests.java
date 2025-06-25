36c2e4a6ea1fcd5661ac536f6ae4d5410064eb98
aungphy0 <aphyo2323@gmail.com>
2025-06-24 19:32:30 -0700

    unit testing for CURD

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
@@ -24,35 +24,60 @@ public class UserRepositoryTests {
     @Autowired
     private TestEntityManager entityManager;
 
-    // @Test
-    // public void testCreateUser(){
-    //     Role roleAdmin = entityManager.find(Role.class, 1);
-    //     User user1 = new User("aphyo2323@gmail.com", "1234", "Aung", "Phyo");
-    //     user1.addRole(roleAdmin);
+    @Test
+    public void testCreateUser(){
+        Role roleAdmin = entityManager.find(Role.class, 1);
+        User user1 = new User("aphyo2323@gmail.com", "1234", "Aung", "Phyo");
+        user1.addRole(roleAdmin);
 
-    //     User savedUser = repo.save(user1);
+        User savedUser = repo.save(user1);
 
-    //     assertThat(savedUser.getId()).isGreaterThan(0);
-    // }
+        assertThat(savedUser.getId()).isGreaterThan(0);
+    }
 
-    // @Test
-    // public void testCreateNewUserWithTwoRoles(){
-    //     User userRavi = new User("ravi@gmail.com", "ravi2020", "Ravi", "Kumar");
-    //     Role roleEditor = new Role(3);
-    //     Role roleAssistant = new Role(5);
+    @Test
+    public void testCreateNewUserWithTwoRoles(){
+        User userRavi = new User("ravi@gmail.com", "ravi2020", "Ravi", "Kumar");
+        Role roleEditor = new Role(3);
+        Role roleAssistant = new Role(5);
 
-    //     userRavi.addRole(roleEditor);
-    //     userRavi.addRole(roleAssistant);
+        userRavi.addRole(roleEditor);
+        userRavi.addRole(roleAssistant);
 
-    //     User savedUser = repo.save(userRavi);
+        User savedUser = repo.save(userRavi);
 
-    //     assertThat(savedUser.getId()).isGreaterThan(0);
-    // }
+        assertThat(savedUser.getId()).isGreaterThan(0);
+    }
 
     @Test
     public void testListAllUsers(){
         Iterable<User> listUsers = repo.findAll();
         listUsers.forEach(user -> System.out.println(user));
+    }
+
+    @Test
+    public void testGetUserById(){
+        User username = repo.findById(1).get();
+        System.out.println(username);
+        assertThat(username).isNotNull();
+    }
+
+    @Test
+    public void testUpdateUserRoles(){
+        User userRavi = repo.findById(2).get();
+        Role roleEditor = new Role(3);
+        Role roleSalesperson = new Role(2);
+
+        userRavi.getRoles().remove(roleEditor);
+        userRavi.addRole(roleSalesperson);
+
+        repo.save(userRavi);
+    }
+
+    @Test
+    public void testDeleteUser(){
+        Integer userId = 2;
+        repo.deleteById(userId);
     }
     
 }
